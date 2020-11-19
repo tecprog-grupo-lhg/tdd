72665c1b83f556d3fddbdbc56051068b5142d869
Guilherme de Lyra <guilyra12@gmail.com>
2020-11-19 20:03:57 -0300

    adding main functionalities
    Co-authored-by: Henrique Martins <henrikem100@gmail.com>
    Co-authored-by: LuisCL94 <lclaudio.tl@gmail.com>

--- src/main/java/com/tecprog/App.java
+++ src/main/java/com/tecprog/App.java
@@ -1,9 +1,4 @@
 package com.tecprog;
-
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
---
+++ src/main/java/com/tecprog/Parser.java
@@ -0,0 +1,51 @@
+import java.io.File;
+import java.util.Scanner;
+import java.nio.file.Path;
+
+public class Parser {
+	private File file;
+	private char delimiter;
+	private Path output;
+	private String format;
+	
+	public void setFile(String fileName) {
+    // not implemented yet
+	}
+	
+	public void setDelimiter(String delimiter) {
+    // not implemented yet
+	}
+	
+	public void setOutput(String output) {
+    // not implemented yet
+	}
+	
+	public void setFormat(String format) {
+    // not implemented yet
+  }
+	
+	public void parse() {
+    // not implemented yet
+  }
+	
+	public void save() {
+    // not implemented yet
+  }
+	
+	private void saveWithColumnFormat() {
+    // not implemented yet
+  }
+	
+	private void saveWithRowFormat() {
+    // not implemented yet
+  }
+	
+	private void checkIfFileExists(File f) {
+    // not implemented yet
+	}
+	
+	private char transformDelimiterToChar(String delimiter) {
+    // not implemented yet
+    return 'a';
+	}
+}
