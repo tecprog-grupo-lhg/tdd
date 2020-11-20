fa6087c6fb1ac132e53293b18f021fef81b082b2
Guilherme de Lyra <guilyra12@gmail.com>
2020-11-19 21:43:24 -0300

    adding unit test for 'setFile' method [which is located at Parser class]
    Co-authored-by: Henrique Martins <henrikem100@gmail.com>
    Co-authored-by: LuisCL94 <lclaudio.tl@gmail.com>

--- src/test/java/com/tecprog/ParserTest.java
+++ src/test/java/com/tecprog/ParserTest.java
@@ -7,14 +7,12 @@ import org.junit.jupiter.api.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class ParserTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void setFile() {
+      // not implemented yet
+      fail("not yet implemented");
     }
+
 }
